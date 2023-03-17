cab294ef7feb51fefb86c72461e1c55d14c73e3c
João Rocha <1220256@isep.ipp.pt>
2023-03-17 22:45:21 +0000

    Cleaned Project and prepared to create vote service commands, close #2

--- src/main/java/com/isep/acme/ACMEApplication.java
+++ src/main/java/com/isep/acme/ACMEApplication.java
@@ -2,19 +2,13 @@ package com.isep.acme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.BufferedImageHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 
-import com.isep.acme.property.FileStorageProperties;
-
 import java.awt.image.BufferedImage;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-		FileStorageProperties.class
-})
 public class ACMEApplication {
 
 	public static void main(String[] args) {
